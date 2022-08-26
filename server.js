d3c442c36b4152a69fb0b3239103979b3bbd289b
Pratik Singh Thakur <pratikthakur2019@gmail.com>
2022-08-26 07:50:10 +0530

    updated the serverjs with www in getUrl, steps, points, url route

--- server.js
+++ server.js
@@ -84,7 +84,7 @@ app.post('/url', (req, res) => {
     const oauth2Client = new google.auth.OAuth2(
         "611658826728-ob0ffv5qe6gee0o4q32afip1ldb71632.apps.googleusercontent.com",
         "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",
-        "https://fitness-game-server.heroku/points"
+        "https://www.fitness-game-server.heroku/points"
     );
 
     const scopes = ["https://www.googleapis.com/auth/fitness.activity.read profile email openid"]
@@ -127,7 +127,7 @@ app.get('/points', async(req, res) => {
                 const oauth2Client = new google.auth.OAuth2(
                     "611658826728-ob0ffv5qe6gee0o4q32afip1ldb71632.apps.googleusercontent.com",
                     "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",
-                    "https://fitness-game-server.heroku.com/points",
+                    "https://www.fitness-game-server.heroku.com/points",
                     true
                 );
                 const tokens = await oauth2Client.getToken(code);
@@ -262,7 +262,7 @@ app.post('/getURL', (req, res) => {
     const oauth2Client = new google.auth.OAuth2(
         "611658826728-ob0ffv5qe6gee0o4q32afip1ldb71632.apps.googleusercontent.com",
         "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",
-        "https://fitness-game-server.herokuapp.com/steps",
+        "https://www.fitness-game-server.herokuapp.com/steps",
         true
     );
 
@@ -305,7 +305,7 @@ app.get('/steps', async (req, res) => {
                 const oauth2Client = new google.auth.OAuth2(
                     "611658826728-ob0ffv5qe6gee0o4q32afip1ldb71632.apps.googleusercontent.com",
                     "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",
-                    "https://fitness-game-server.heroku/steps",
+                    "https://www.fitness-game-server.heroku/steps",
                     true
                 );
                 const tokens = await oauth2Client.getToken(code);
