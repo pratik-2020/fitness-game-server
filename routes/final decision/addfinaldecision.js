42921cc40066c434cf605401145f1e32b5622c59
Pratik Singh Thakur <pratikthakur2019@gmail.com>
2022-08-30 08:25:25 +0530

    added email in addfinal module

--- routes/final decision/addfinaldecision.js
+++ routes/final decision/addfinaldecision.js
@@ -1,36 +1,42 @@
 const finaldecisionModel = require('../../model/finaldecison');
 const groupModel = require('../../model/group');
+const userModel = require('../../model/user');
 const addFinalDecision = (req, res) => {
     const grpid = req.body.grpid;
     const decision = req.body.decision;
     const description = req.body.description;
-    groupModel.find({
-        grpid: grpid
-    }).then((response) => {
-        if(response.length > 0){
-            let finalm = new finaldecisionModel();
-            finalm.grpid = grpid;
-            finalm.level = response[0].currentLevel;
-            finalm.decision = decision;
-            finalm.description = description;
-            finalm.save((err, data) => {
-                if(err){
-                    console.log('Er1');
-                    console.log(err);
-                    console.log(err);
-                }
-                else{
-                    res.send('Decision added');
-                }
-            })
-        }
-        else{
-            console.log('Grp DNE');
-        }
-    }).catch((er) => {
-        console.log('Er2');
-        console.log(er);
-        console.log(er);
+    const email = req.body.email;
+    userModel.find({
+        email: email
+    }).then((rep1) => {
+        groupModel.find({
+            grpid: rep1[0].grpid
+        }).then((response) => {
+            if(response.length > 0){
+                let finalm = new finaldecisionModel();
+                finalm.grpid = grpid;
+                finalm.level = response[0].currentLevel;
+                finalm.decision = decision;
+                finalm.description = description;
+                finalm.save((err, data) => {
+                    if(err){
+                        console.log('Er1');
+                        console.log(err);
+                        console.log(err);
+                    }
+                    else{
+                        res.send('Decision added');
+                    }
+                })
+            }
+            else{
+                console.log('Grp DNE');
+            }
+        }).catch((er) => {
+            console.log('Er2');
+            console.log(er);
+            console.log(er);
+        })
     })
 }
 
